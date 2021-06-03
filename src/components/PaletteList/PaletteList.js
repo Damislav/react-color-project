24be3f7cb6bbbe4b6368eaee416863c783b98f6f
Damislav <idamjanovic28@gmail.com>
2021-06-03 22:22:42 +0200

    #

--- src/components/PaletteList/PaletteList.js
+++ src/components/PaletteList/PaletteList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import MiniPalette from "../MiniPalette/MiniPalette";
 import { withStyles } from "@material-ui/styles";
+import chroma from "chroma-js";
 
 const styles = {
   root: {
