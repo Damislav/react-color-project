5f59d5ed79ac2e87bdd06844e80544b15dbba07a
Damislav <idamjanovic28@gmail.com>
2021-06-04 22:34:21 +0200

    add save palette

--- src/App.js
+++ src/App.js
@@ -3,19 +3,39 @@ import { generatePalette } from "./colorHelpers";
 import seedColors from "./seedColors";
 import { Route, Switch } from "react-router-dom";
 import SingleColorPalette from "./components/SingleColorPalette/SingleColorPalette";
+
 import PaletteList from "./components/PaletteList/PaletteList";
 import Palette from "./components/Palette/Palette";
 import NewPaletteForm from "./components/NewPaletteForm/NewPaletteForm";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { palettes: seedColors };
+    this.savePalette = this.savePalette.bind(this);
+    this.findPalette = this.findPalette.bind(this);
+  }
   findPalette(id) {
     return seedColors.find((palette) => {
       return palette.id === id;
     });
   }
+  savePalette(newPalette) {
+    this.setState({ palettes: [...this.state.palettes, newPalette] });
+  }
   render() {
     return (
       <Switch>
-        <Route path="/palette/new" render={() => <NewPaletteForm />} />
+        <Route
+          path="/palette/new"
+          render={(routeProps) => (
+            <NewPaletteForm
+              savePalette={this.savePalette}
+              palettes={this.state.palettes}
+              {...routeProps}
+            />
+          )}
+        />
         <Route
           exact
           path="/palette/:paletteId/:colorId"
@@ -32,7 +52,7 @@ class App extends Component {
           exact
           path="/"
           render={(routeProps) => (
-            <PaletteList palettes={seedColors} {...routeProps} />
+            <PaletteList palettes={this.state.palettes} {...routeProps} />
           )}
         />
         <Route
